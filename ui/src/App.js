d19cda721e8c7c05acd0f8dcf5d97f75bd1bd2d4
OTod <Oleks.todorov@gmail.com>
2022-07-27 13:30:22 +0300

    IC-11 added simple pictures component

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import PictureHolder from './components/PictureHolder/PictureHolder';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Cheer Up! =)</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <PictureHolder></PictureHolder>
+    
       </div>
     );
   }
